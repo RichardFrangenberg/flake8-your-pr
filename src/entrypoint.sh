cfc4193ce4bda57e7f2fa608a41261870d004672
RichardFrangenberg <richardf.1@web.de>
2019-11-10 15:39:29 +1030

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -57,7 +57,7 @@ main() {
 #    echo "New files in branch: $new_files_in_branch"
     # Feed to flake8 which will return the output in json format.
     # shellcheck disable=SC2086
-#    flake8 --format=json $new_files_in_branch | jq '.' > flake8_output.json || true # NOQA
+    flake8 --format=json . | jq '.' > flake8_output.json || true # NOQA
     python /src/main.py
 }
 
